f8083e8a36181df8779d7e41abdfb69a914d7e20
BenjaminLai05 <a1888072@adelaide.edu.au>
2023-07-25 21:41:00 +0930

    commit

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-using namespace std;
 
 extern int array_sum(int array[], int n);
 
 int main() {
   int array[6] = {1, 2, 3, 4, 5, 6};
 
-  cout << "Sum of array elements: " << array_sum(array, 6);
+  std::cout << "Sum of array elements: " << array_sum(array, 6) << std::endl;
+  
+  return 0;
 }
