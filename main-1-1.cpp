cc1637ef055a1f750399a2e684d48993400365f2
BenjaminLai05 <a1888072@adelaide.edu.au>
2023-07-25 21:34:44 +0930

    modified

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern int array_sum(int array[], int n);
 
 int main() {
-  int array[5] = {1, 2, 3, 4, 5};
+  int array[6] = {1, 2, 3, 4, 5, 6};
 
-  cout << "Sum of array elements: " << array_sum(array, 5);
+  cout << "Sum of array elements: " << array_sum(array, 6);
 }
