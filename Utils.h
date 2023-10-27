670312f0d8dbdda8d4872eae8da8d0c88e9aed2a
BenjaminLai05 <a1888072@adelaide.edu.au>
2023-10-27 14:34:06 +1030

    commit

--- Utils.h
+++ Utils.h
@@ -3,24 +3,28 @@
 
 #include <tuple>
 #include <cmath>
-#include <cstdlib> // for rand()
+#include <iostream>
 
 class Utils {
-public:
-    static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
-        int x = rand() % gridWidth;
-        int y = rand() % gridHeight;
-        return std::make_tuple(x, y);
-    }
+   private: 
+   
+   public:
+   static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
+      int x = rand() % gridWidth; 
+      int y = rand() % gridHeight;
+      return std::make_tuple(x,y);
+   }
 
-    static double calculateDistance(const std::tuple<int, int>& pos1, const std::tuple<int, int>& pos2) {
+   double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2) {
         int x1 = std::get<0>(pos1);
         int y1 = std::get<1>(pos1);
         int x2 = std::get<0>(pos2);
         int y2 = std::get<1>(pos2);
 
         return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
-    }
+   }
+
+   
 };
 
 #endif
