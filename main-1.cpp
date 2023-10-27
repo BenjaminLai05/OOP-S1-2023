13ea0ec84bc940dc351f4151405928a70be31a05
BenjaminLai05 <a1888072@adelaide.edu.au>
2023-10-27 14:45:46 +1030

    commiting

--- main-1.cpp
+++ main-1.cpp
@@ -2,7 +2,7 @@
 #include <ctime>
 #include <tuple>
 
-#include "Cell.h"
+/*#include "Cell.h"
 #include "Effect.h"
 #include "Utils.h"
 
@@ -12,5 +12,26 @@ int main() {
     auto position1 = Utils::generateRandomPos(10, 10);
     auto position2 = Utils::generateRandomPos(10, 10);
 
+    Utils::calculateDistance(position1, position2);
+
+    return 0;
+}*/
+
+#include <iostream>
+#include "Cell.h"
+#include "Utils.h"
+
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    auto position1 = Utils::generateRandomPos(10, 10);
+    auto position2 = Utils::generateRandomPos(10, 10);
+
+    std::cout << "Position 1: (" << std::get<0>(position1) << ", " << std::get<1>(position1) << ")\n";
+    std::cout << "Position 2: (" << std::get<0>(position2) << ", " << std::get<1>(position2) << ")\n";
+
+    std::cout << "Distance between positions: " << Utils::calculateDistance(position1, position2) << std::endl;
+
     return 0;
 }
+
