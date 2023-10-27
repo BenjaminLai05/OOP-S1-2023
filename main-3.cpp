905d207dfe6540074b82ffadc604e5795b2adc97
BenjaminLai05 <a1888072@adelaide.edu.au>
2023-10-27 15:35:53 +1030

    commit

--- main-3.cpp
+++ main-3.cpp
@@ -2,7 +2,5 @@
 
 int main() {
     Game game;
-    game.initGame(3, 2, 10, 10); // Initialize with some characters and traps
-    game.gameLoop(100, 1.5); // Start the game loop with a maximum of 100 iterations
     return 0;
 }
